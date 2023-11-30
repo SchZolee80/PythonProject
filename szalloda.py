faf3f4c75abbb001e5debbb8c57a549b83786e24
SchZolee80 <sch.zolee80@gmail.com>
2023-11-30 07:52:52 +0100

    Szalloda Osztályok létrehozása

--- szalloda.py
+++ szalloda.py
@@ -1,9 +1,9 @@
 class Szoba:
     """A szálloda szobáját reprezentáló osztály"""
 
-    def __init__(self, szobaszam, foglalt=False):
+    def __init__(self, szobaszam: int, foglalt=False):
         """Inicializálja a szoba attribútumait"""
-        self.szobaszam = szobaszam
+        self.szobaszam = int(szobaszam)
         self.foglalt = foglalt
         self.ar = 0  # Az árat inicializáljuk 0-val, majd a különböző típusokban felülírjuk
         self.extrak = []  # Az extrák listáját inicializáljuk üres listával
@@ -54,3 +54,68 @@ class Lakosztaly(Szoba):
         super().__init__(szobaszam, foglalt)
         self.ar = 30000  # Ár inicializálása
         self.extrak = ["TV", "WIFI", "Minibár"]  # Extra tulajdonságok inicializálása
+
+class SzallodaRendszer:
+    """A szálloda rendszerét kezelő osztály"""
+
+    def __init__(self):
+        """Inicializálja a szálloda rendszerét"""
+        self.szobak = []  # Szobák listája
+
+    def adatfeltolto(self):
+        """Feltölti az adatokat néhány alapértelmezett szobával"""
+        self.szobak.append(KetagyasSzoba(szobaszam=101))
+        self.szobak.append(EgyagyasSzoba(szobaszam=102))
+        self.szobak.append(Lakosztaly(szobaszam=201))
+
+    def foglalas_lekerdez(self):
+        """Lekérdezi a foglalásokat"""
+        for szoba in self.szobak:
+            if szoba.foglalt:
+                print(f"Szoba {szoba.szobaszam} foglalt.")
+            else:
+                print(f"Szoba {szoba.szobaszam} szabad.")
+
+    def arak_lekerdez(self):
+        """Lekérdezi a szobák árait"""
+        for szoba in self.szobak:
+            szoba.ar_megjelenites()
+
+    def foglalas_lead(self, szobaszam: int):
+        """Foglalást hajt végre a megadott szobaszámon"""
+        szobaszam = int(szobaszam)
+        for szoba in self.szobak:
+            if szoba.szobaszam == szobaszam:
+                szoba.foglal()
+                return True
+        print(f"Az adott szobaszám ({szobaszam}) nem található.")
+        return False
+
+# Tesztelés
+rendsz = SzallodaRendszer()
+rendsz.adatfeltolto()
+
+while True:
+    print("\nVálasszon egy opciót:")
+    print("1. Foglalások lekérdezése")
+    print("2. Árak lekérdezése")
+    print("3. Új foglalás leadása")
+    print("4. Kilépés")
+
+    valasztas = input("Adja meg a választott opció számát: ")
+
+    if valasztas == "1":
+        rendsz.foglalas_lekerdez()
+    elif valasztas == "2":
+        rendsz.arak_lekerdez()
+    elif valasztas == "3":
+        szobaszam = input("Adja meg a foglalni kívánt szobaszámot: ")
+        if rendsz.foglalas_lead(szobaszam):
+            print(f"A szoba {szobaszam} sikeresen foglalva.")
+        else:
+            print("A foglalás sikertelen.")
+    elif valasztas == "4":
+        print("Viszontlát!")
+        break
+    else:
+        print("Érvénytelen választás. Kérem, válasszon újra.")
